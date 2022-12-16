59dbbaedb9b1fd54d46778c8b4e49063eabf9afa
beerpiss <lacvtg.a1.2023@gmail.com>
2022-12-16 08:50:54 +0700

    Updates

---
+++ _app/immutable/chunks/2-0c93eca7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-97f0d4a2.js";export{t as component};
--- _app/immutable/chunks/2-cd8d0838.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-be3ab387.js";export{t as component};
